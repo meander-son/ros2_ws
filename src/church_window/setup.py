87a3235deb9460b7ad35e35c446c81d2de5b7a5e
Mark Anderson <merk12anderson@gmail.com>
2026-07-08 16:24:31 +0100

    NDT_Church_Window

---
+++ src/church_window/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'church_window'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        #(os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mark',
+    maintainer_email='mark@todo.todo',
+    description='Zivid Artist Bot using rembg and continuous raycasting',
+    license='TODO: License declaration',
+    tests_require=['pytest']
+)
